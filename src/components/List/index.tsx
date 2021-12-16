cb646de8591060d25587e0a75d8af8dadd67511a
scvd03 <scvd03@gmail.com>
2021-12-16 14:28:51 +0900

    🔥  Remove: 중복코드 제거

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -3,16 +3,9 @@ import styled from '@emotion/styled';
 import Todo from './Todo';
 import { todoSelector, todos } from '../../atoms';
 import { useRecoilValue } from 'recoil';
-import { useEffect } from 'react';
 
 export default function List() {
   const filterdTodoList = useRecoilValue(todoSelector);
-  const todoList = useRecoilValue(todos);
-  // Q: todoList를 구독하고 있는 컴포넌트에서 상태가 변경되었을때 로컬스토리지를 업데이트. 이게 최선??
-  useEffect(() => {
-    // !update
-    window.localStorage.setItem('todos', JSON.stringify(todoList));
-  }, [todoList]);
 
   return (
     <Container>
